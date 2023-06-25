ac193628919a9728a1505a9b631ec1b8ea13356f
Ahmed Alexander <92260103+aalxer@users.noreply.github.com>
2023-06-25 17:48:07 +0200

    first try DB Login

--- controller/controller.js
+++ controller/controller.js
@@ -1,16 +1,7 @@
 let MongoClient = require('mongodb').MongoClient;
-
+let db
 const url = "mongodb://ahmadb_ahma:XU5N7NXCL@mongodb1.f4.htw-berlin.de:27017/ahmadb?authMechanism=DEFAULT"
-
-
-MongoClient.connect (url, function (err, client) {
-    if(err) throw err;
-    let db = client.db("testDB");
-    //Write code to create a collection
-    //Write code to query database
-    //Write code to insert, update, delete documents
-    client.close();
-    });
+let usersFromDB
 
 let users = [{
     "userName": "admina",
@@ -62,32 +53,56 @@ let locationsList = [{
 
 exports.login = function (request, response) {
 
-    console.log("controller got login request ..")
-    // get the data from requestObject:
-    let data = request.body
-    console.log("Controller checking the user " + data.userName + " ..")
+    MongoClient.connect(url, function (err, client) {
 
-    for (const user of users) {
-        if (data.userName === user.userName) {
-            console.log("controller found the username ..")
-            if (data.password === user.password) {
-                console.log("controller found the password ..")
-                // send response:
-                let responseData = {
-                    message: 'login successfully',
-                    data: { userName: user.userName, admin: user.admin }
+        console.log("connection to db ..")
+        if(err) {
+            console.log("error on connecting to db")
+            throw err;
+        }
+        console.log("connection to DB : " + client.isConnected)
+        console.log("connection to DB established")
+        db = client.db("ahmadb");
+
+        // --------------------------------------
+
+        console.log("controller got login request ..")
+        // get the data from requestObject:
+        let data = request.body
+        console.log("Controller checking the user " + data.userName + " ..")
+
+        db.collection("users").find({}).toArray((err, res) => {
+            usersFromDB = res
+            console.log("Users got from DB")
+            console.log(usersFromDB.toString())
+        })
+
+        for (const user of usersFromDB) {
+            if (data.userName === user.userId) {
+                console.log("controller found the username ..")
+                if (data.password === user.password) {
+                    console.log("controller found the password ..")
+                    // send response:
+                    let responseData = {
+                        message: 'login successfully',
+                        data: { userName: user.userId, admin: user.role }
+                    }
+                    response.status(200).json(responseData)
+                    return
+                } else {
+                    // send error response:
+                    response.status(500).json({message: "wrong password"})
+                    return
                 }
-                response.status(200).json(responseData)
-                return
-            } else {
-                // send error response:
-                response.status(500).json({message: "wrong password"})
-                return
             }
         }
-    }
-    // send error response:
-    response.status(500).json({"message": "user is not registered"})
+        // send error response:
+        response.status(500).json({"message": "user is not registered"})
+
+        // --------------------------------------
+
+        client.close();
+    });
 }
 
 exports.deleteLocation = function (locationNameToDelete, response) {
